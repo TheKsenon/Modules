9ccded6c3f2de7fca9621c54b7af42b569137898
Ksenon <139338488+TheKsenon@users.noreply.github.com>
2024-01-29 12:47:39 +0500

    Update and rename sdxl2.py to sdxl3.py

--- sdxl3.py
+++ sdxl3.py
@@ -1,16 +1,12 @@
 import requests
 from aiogram import Bot, Dispatcher, types
-from aiogram.types import ParseMode
 from aiogram.utils import executor
 
 API_TOKEN = '6869308437:AAGUE5NM7TgI7kSCJwFdpUid3UQqEx8QDNA'
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-@dp.message_handler(commands=['sdxl'])
-async def process_sdxl_command(message: types.Message):
-    prompt = message.text.split('/sdxl ', 1)[1]
-    
+async def process_sdxl_command(message: types.Message, prompt: str):
     # Отправляем сообщение "Изображение готовится"
     msg_processing = await message.reply(f"Изображение по запросу '{prompt}' готовится...")
 
@@ -39,16 +35,21 @@ async def process_sdxl_command(message: types.Message):
             # Если произошла ошибка запроса, удаляем предыдущее сообщение с ошибкой
             await msg_processing.delete()
             
-            # Отправляем новое сообщение "Изображение готовится"
-            msg_processing = await message.reply(f"Изображение по запросу '{prompt}' готовится...")
-
-            # Пропускаем остальной код, чтобы не отправлять сообщение об ошибке
+            # Продолжаем цикл
             continue
 
         except Exception as e:
-            # Если произошла другая ошибка, отправляем сообщение с описанием ошибки
-            await message.reply(f"Произошла ошибка: {str(e)}", parse_mode=ParseMode.MARKDOWN)
+            # Если произошла другая ошибка, отправляем сообщение с описанием ошибки и завершаем цикл
+            await message.reply(f"Произошла ошибка: {str(e)}")
             break
+
+@dp.message_handler(commands=['sdxl'])
+async def handle_sdxl_command(message: types.Message):
+    try:
+        prompt = message.text.split('/sdxl ', 1)[1]
+        await process_sdxl_command(message, prompt)
+    except IndexError:
+        await message.reply("Пожалуйста, укажите запрос после /sdxl.")
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
